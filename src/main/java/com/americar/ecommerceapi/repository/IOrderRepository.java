1fe19c43ea9e92dfe5e0fd3f224aa42fbaf6294c
Frederick Mejia <fjdevel@gmail.com>
2022-12-22 20:49:41 -0600

    manejo de ordenes terminado

--- src/main/java/com/americar/ecommerceapi/repository/IOrderRepository.java
+++ src/main/java/com/americar/ecommerceapi/repository/IOrderRepository.java
@@ -1,18 +1,11 @@
 package com.americar.ecommerceapi.repository;
 
-import com.americar.ecommerceapi.entity.Item;
+
 import com.americar.ecommerceapi.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Date;
-import java.util.List;
 
 
 public interface IOrderRepository extends JpaRepository<Order,String> {
-
-
 
 }
---
+++ src/main/java/com/americar/ecommerceapi/seeders/OrderSeeder.java
@@ -0,0 +1,43 @@
+package com.americar.ecommerceapi.seeders;
+
+import com.americar.ecommerceapi.entity.Order;
+import com.americar.ecommerceapi.repository.IOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class OrderSeeder implements CommandLineRunner {
+
+    private IOrderRepository orderRepository;
+
+    @Autowired
+    public OrderSeeder(IOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Order order1 = new Order();
+        order1.setId(UUID.randomUUID().toString());
+        order1.setComments("Comentarios del pedido 1");
+        order1.setCustomerId("C1");
+        order1.setDate(new Date());
+        order1.setNumber("123");
+        order1.setOrderTypeId("T1");
+        order1.setPlateNumber("ABC123");
+        order1.setSellerId("S1");
+        order1.setShippingId("SH1");
+        order1.setVin("123ABC");
+        order1.setWarehouseId("W1");
+        order1.setUserDms("U1");
+        order1.setBlockRoute(false);
+        order1.setFullShipment(true);
+        order1.setRoute("Ruta 1");
+
+        orderRepository.save(order1);
+    }
+}
