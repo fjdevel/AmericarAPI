049e204db8011da0eef8bca28d7f7f9306674ffc
Frederick Mejia <frederickm_safi@latmobile.com>
2023-03-24 15:42:52 -0600

    agregando la collection

--- src/main/java/com/americar/ecommerceapi/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.americar.ecommerceapi.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
